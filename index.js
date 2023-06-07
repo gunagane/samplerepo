3ae5464a6688cd0a57bf11aaf5d22ebf0af94b83
Ganesh <ganeshguna8@gmail.com>
2023-06-07 17:23:03 +0530

    sample_Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const data = {
+    name:"ganesh",
+    age:"28",
+    bloodGroup:"O+ve",
+    village:"Varighedu",
+    poincode:"534225",
+    phone:"898586545855",
+    street : "sivalayam street"
+}
